aac9a301dabcf7493e31b9f40dc699f25d8cafea
Krtosh <51253577+Krtosh@users.noreply.github.com>
2026-05-13 15:41:46 +0300

    server.js

--- server.js
+++ server.js
@@ -81,6 +81,8 @@ app.post("/summarize", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
